fac0a3a6c77cab947c3969df0335328a8066ee05
YashinSN1 <akbarikhaton07@gmail.com>
2026-06-14 07:00:32 +0530

    changed auth they were reverse their role justn oticee

--- ServerSide/src/routes/App.Router.js
+++ ServerSide/src/routes/App.Router.js
@@ -4,12 +4,12 @@ import AppController from '../controller/App.Controller.js';
 import ValidateSchema from '../middleware/Schema.Validator.js'
 import HabitSchema from '../middleware/Habit.Schema.Joi.js'
 import { CreateHabit, DeleteHabit, UpdateMyHabit, GetMyHabits } from '../controller/Habit.Crud.js';
-import AuthenticateUser from '../middleware/Authenticate.User.js';
+import AuthorizeUser from '../middleware/Authorize.User.js';
 
-router.get('/app', AuthenticateUser, AppController);
-router.post('/app/newhabit', AuthenticateUser, ValidateSchema(HabitSchema), CreateHabit);
-router.get('/app/habits', AuthenticateUser, GetMyHabits);
-router.delete('/app/habits/:habitId', AuthenticateUser, DeleteHabit)
-router.patch('/app/habits/:habitId', AuthenticateUser, UpdateMyHabit)
+router.get('/app', AuthorizeUser, AppController);
+router.post('/app/newhabit', AuthorizeUser, ValidateSchema(HabitSchema), CreateHabit);
+router.get('/app/habits', AuthorizeUser, GetMyHabits);
+router.delete('/app/habits/:habitId', AuthorizeUser, DeleteHabit)
+router.patch('/app/habits/:habitId', AuthorizeUser, UpdateMyHabit)
 
 export default router;
--- ServerSide/src/routes/App.Tracking.js
+++ ServerSide/src/routes/App.Tracking.js
@@ -1,14 +1,14 @@
 import express from 'express';
 const router = express.Router();
-import AuthenticateUser from '../middleware/Authenticate.User.js';
+import AuthorizeUser from '../middleware/Authorize.User.js';
 import ValidateSchema from '../middleware/Schema.Validator.js';
 import TrackHabitData from '../middleware/Track.Habit.Joi.js';
 import { TrackHabitRecord, GetHabitTrackingData, UpdateHabitTrackingRecord } from '../controller/Track.Habit.Controller.js';
 
-router.post('/app/habit/createTracking/:Habitid', AuthenticateUser, ValidateSchema(TrackHabitData), TrackHabitRecord);
-router.get('/app/habit/tracking', AuthenticateUser, GetHabitTrackingData)
-router.get('/app/habit/tracking/{:HabitTrackDate}', AuthenticateUser, GetHabitTrackingData);
-router.patch('/app/habit/updateTracking/:Habitid', AuthenticateUser, ValidateSchema(TrackHabitData), UpdateHabitTrackingRecord);
+router.post('/app/habit/createTracking/:Habitid', AuthorizeUser, ValidateSchema(TrackHabitData), TrackHabitRecord);
+router.get('/app/habit/tracking', AuthorizeUser, GetHabitTrackingData)
+router.get('/app/habit/tracking/{:HabitTrackDate}', AuthorizeUser, GetHabitTrackingData);
+router.patch('/app/habit/updateTracking/:Habitid', AuthorizeUser, ValidateSchema(TrackHabitData), UpdateHabitTrackingRecord);
 
 
 export default router;
