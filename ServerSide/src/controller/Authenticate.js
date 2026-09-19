5199e12f8d71382d70f7825e3153a57fba731093
YashinS1 <akbarikhaton07@gmail.com>
2026-09-19 22:05:50 +0530

    finally everythings is fixed backend and fronted connection formed on vercel and fixed cookie issue on production

--- ServerSide/src/controller/Authenticate.js
+++ ServerSide/src/controller/Authenticate.js
@@ -104,10 +104,13 @@ export const LoginUser = async (req, res) => {
                 expiresIn: '7d'
             }
         );
-        res.cookie('token', token, {
+
+        const isProduction = process.env.NODE_ENV === "production";
+
+        res.cookie("token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production' || false,
-            sameSite: 'strict',
+            secure: isProduction,
+            sameSite: isProduction ? "none" : "lax",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
